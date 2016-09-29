b385a064f4676bb715b887b6cb57f976b0ec2950
Eugene <Beetr@EUGENE_S_PC>
2016-09-29 21:37:24 +0300

    Trying make manage view

--- TaskManager/Controllers/ProjectController.cs
+++ TaskManager/Controllers/ProjectController.cs
@@ -154,5 +154,58 @@ namespace TaskManager.Controllers
         {
             return RedirectToAction("UserEmails", "Account");
         }
+
+        [Authorize(Roles = "Administrator, Manager, User")]
+        [HttpGet]
+        public ActionResult Manage()
+        {
+            var manageModel = new ManageModel();
+            if (User.IsInRole("Administrator") || User.IsInRole("Manager"))
+            {
+                manageModel.ProjectActions = new List<string>()
+                {
+                    "Add user to project",
+                    "Delete project",
+                    "Manage tasks"
+                };
+                manageModel.TaskAction = new List<string>()
+                {
+                    "Assign to user",
+                    "Delete"
+                };
+            }
+            else
+            {
+                manageModel.ProjectActions = new List<string>()
+                {
+                    "Delete project",
+                    "Manage tasks"
+                };
+                manageModel.TaskAction = new List<string>()
+                {
+                    "Delete"
+                };
+            }
+            var projectService = new ProjectService();
+            TempData["ProjectTitles"] = projectService.GetUserProjects(User.Identity.GetUserId()).Select(x => x.Title).ToList();
+            return PartialView("Manage", manageModel);
+        }
+
+        [Authorize(Roles = "Administrator, Manager, User")]
+        [HttpPost]
+        public ActionResult Manage(ManageModel model)
+        {
+            return PartialView("Manage");
+        }
+
+        [Authorize(Roles = "Administrator, Manager, User")]
+        [HttpGet]
+        public ActionResult ProjectList()
+        {
+            var projectService = new ProjectService();
+            var projecttitles = projectService.GetUserProjects(User.Identity.GetUserId()).Select(x => x.Title).ToList();
+            return Json(projecttitles, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ TaskManager/Models/MangeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class ManageModel
+    {
+        public string ProjectTitle { get; set; }
+        public string TaskTitle { get; set; }
+        public string AssignToEmail { get; set; }
+
+        public List<string> ProjectActions { get; set; }
+        public List<string> TaskAction { get; set; }
+    }
+}
